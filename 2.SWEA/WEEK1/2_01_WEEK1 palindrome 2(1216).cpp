48c223a4fbbe9aafb322f7bd8acce7a824dddd4c
WONILLISM <wonillism@gmail.com>
2019-07-15 17:52:20 +0900

    complete WEEK1 problems

--- 2.SWEA/WEEK1/2_01_WEEK1 palindrome 2(1216).cpp
+++ 2.SWEA/WEEK1/2_01_WEEK1 palindrome 2(1216).cpp
@@ -3,52 +3,52 @@ char Map[101][101];
 int MaxLen;
 void Init() {
 	MaxLen = 0;
-	for(int i = 0; i < 100; i++) {
+	for (int i = 0; i < 100; i++) {
 		for (int j = 0; j < 100; j++) {
-			Map[i][j] = 0;
+			Map[i][j] = '\0';
 		}
 	}
 }
 void Process() {
 	for (int i = 0; i < 100; i++) {
 		for (int j = 0; j < 100; j++) {
-			bool chk = false;
-			for (int k = j; k < 100; k++) {
-				if (Map[i][j] == Map[i][99 - k]) {
-					int len = 99 - k - j + 1;
-					for (int l = 0; l < len / 2; l++) {
-						if (Map[i][j + l] != Map[i][99 - l]) {
-							chk = true;
-							break;
-						}
+			for (int k = 99; k >= j; k--) {	//양 끝이 같은 곳을 찾기 위함
+				bool chk = false;
+				int len = k - j + 1;
+				for (int l = 0; l < len / 2; l++) {	//회문인지 확인하기 위함
+					if (Map[i][j + l] != Map[i][k - l]) {
+						chk = true;
+						break;
 					}
-					if (!chk&&MaxLen < len)MaxLen = len;
 				}
-				if (!chk) break;
+				if (!chk&&MaxLen < len) {
+					MaxLen = len;//!chk가 회문
+					break;//회문을 찾았으니 더이상 돌 필요가 없음
+				}
 			}
 		}
 	}
 	for (int i = 0; i < 100; i++) {
 		for (int j = 0; j < 100; j++) {
-			bool chk = false;
-			for (int k = j; k < 100; k++) {
-				if (Map[j][i] == Map[99 - k][i]) {
-					int len = 99 - k - j + 1;
-					for (int l = 0; l < len / 2; l++) {
-						if (Map[j + l][i] != Map[99 - l][i]) {
-							chk = true;
-							break;
-						}
+			for (int k = 99; k >= j; k--) {	//양 끝이 같은 곳을 찾기 위함
+				bool chk = false;
+				int len = k - j + 1;
+				for (int l = 0; l < len / 2; l++) {	//회문인지 확인하기 위함
+					if (Map[j + l][i] != Map[k - l][i]) {
+						chk = true;
+						break;
 					}
-					if (!chk&&MaxLen < len)MaxLen = len;
 				}
-				if (!chk) break;
+				if (!chk&&MaxLen < len) {
+					MaxLen = len;
+					break;//회문을 찾았으니 더이상 돌 필요가 없음
+				}
 			}
 		}
 	}
 }
 int main() {
-	freopen("D2(1216).in.txt", "r", stdin);
+	freopen("WEEK1(1216).in.txt", "r", stdin);
 	for (int i = 1; i <= 10; i++) {
 		Init();
 		scanf("%*d");
