b84c66ef98a2b92e28c2586eac7d5e28032f926e
WONILLISM <wonillism@gmail.com>
2019-11-19 13:23:18 +0900

    add some..

---
+++ 1.baekjoon/02_Samsung_EX/01_02_Samsung_EX/01_02_Samsung_EX/29_주사위 윷놀이.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+vector<vector<int>> map;
+void solution() {
+	
+}
+int main() {
+	vector<int> m;
+	m = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 };
+	map.push_back(m);
+	m = { 13, 16, 19, 25, 30, 35, 40 };
+	map.push_back(m);
+	m = { 22, 24, 25, 30, 35, 40 };
+	map.push_back(m);
+	m = { 28, 27, 26, 25, 30, 35, 40 };
+	map.push_back(m);
+	
+	return 0;
+}
