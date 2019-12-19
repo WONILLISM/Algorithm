71f510f9c8c588ddbf0915a0fd55802f51fcada9
WONILLISM <wonillism@gmail.com>
2019-12-19 13:36:23 +0900

    2019-12-18

---
+++ 3.programmers/level3-5/level3-5/level3-5/lv4_몸짱트레이너 라이언의 고민.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<vector<int>> timetable) {
+	int answer = 0;
+	return answer;
+}
+int main() {
+	vector<vector<int>> t = { {1170, 1210}, {1200, 1260} };
+	cout << solution(3, 2, t) << endl;
+	return 0;
+}
