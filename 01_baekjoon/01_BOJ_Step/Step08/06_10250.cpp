ad3589e8c8e20075aac7909c21735ed60938c14c
WONILLISM <wonillism@gmail.com>
2020-01-02 23:43:27 +0900

    Step08_06 modified and Step 09 01 ~ 05 updated

--- 01_baekjoon/01_BOJ_Step/Step08/06_10250.cpp
+++ 01_baekjoon/01_BOJ_Step/Step08/06_10250.cpp
@@ -17,11 +17,14 @@ void solution_10250() {
 	scanf("%d", &t);
 	while (t--) {
 		scanf("%d %d %d", &h, &w, &n);
-		int x = 1;
-		while (n>h) {
-			n -= h;
-			x++;
-		}
-		printf("%d%02d\n", n, x);
+		int row = n % h == 0 ? h : n % h;
+		int col = n % h == 0 ? n / h : (n / h) + 1;
+		printf("%d%02d\n", row, col);
+		//int x = 1;
+		//while (n>h) {
+		//	n -= h;
+		//	x++;
+		//}
+		//printf("%d%02d\n", n, x);
 	}
 }
--- 01_baekjoon/01_BOJ_Step/Step09/03_1929.cpp
+++ 01_baekjoon/01_BOJ_Step/Step09/03_1929.cpp
@@ -7,12 +7,23 @@
 #define ll long long
 using namespace std;
 
-
+int M, N;
+bool Num[1000001];
 void solution_1929();
 int main() {
 	solution_1929();
 	return 0;
 }
 void solution_1929() {
-
+	scanf("%d %d", &M, &N);
+	Num[0] = true;
+	Num[1] = true;
+	int i = 2, j = 2;
+	while (i*j <= 1000000) {
+		for(int j=2; j*i<=1000000; j++)
+			if (!Num[i*j])Num[i*j] = true;
+		i++;
+	}
+	for (int i = M; i <= N; i++)
+		if (!Num[i])printf("%d\n", i);
 }
