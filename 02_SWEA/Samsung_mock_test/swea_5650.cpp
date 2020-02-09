4b26fba40669c6b125249b232bbc82bd2de0d87a
WONILLISM <wonillism@gmail.com>
2020-02-09 19:53:43 +0900

    refer to someone

--- 02_SWEA/Samsung_mock_test/swea_5650.cpp
+++ 02_SWEA/Samsung_mock_test/swea_5650.cpp
@@ -16,15 +16,15 @@ int Map[MAX][MAX];
 int visit[MAX][MAX];
 int N, ans;
 int sx, sy;//시작점
-bool gameover;
 int dx[] = { 1,0,-1,0 };
 int dy[] = { 0,1,0,-1 };
-int convDir[5][4] = {	//블럭 모양에 따른 방향전환
+int convDir[6][4] = {	//블럭 모양에 따른 방향전환
 	{0,0,0,0},
 	{2,0,3,1},
 	{2,3,1,0},
 	{1,3,0,2},
-	{3,2,0,1}
+	{3,2,0,1},
+	{2,3,0,1}
 };
 
 vector<vector<PII>> wormhole;//웜홀 쌍의 위치(y,x)
@@ -39,18 +39,16 @@ struct Pinball {
 
 bool isRange(int y, int x) { return x > 0 && x <= N && y > 0 && y <= N; }
 void process(int y, int x, int dir, int cnt) {
-	if (gameover)return;
 	int ny = y + dy[dir], nx = x + dx[dir];
-	
+
 	int num = Map[ny][nx]; // 웜홀 번호 ,블럭 번호, 빈 공간, 블랙홀
 	if (isRange(ny, nx)) {
 		if (num == -1 || (ny == sy && nx == sx)) {	//종료 조건 처리
 			ans = max(cnt, ans);
-			gameover = true;
 			return;
 		}
-		if (num >= 6 && num <= 10) {		// 웜홀 조건 처리
-			
+		else if (num >= 6 && num <= 10) {		// 웜홀 조건 처리
+
 			int wy, wx; //다음 웜홀 좌표
 			if (ny == wormhole[num - 6][0].first && nx == wormhole[num - 6][0].second)
 				wy = wormhole[num - 6][1].first, wx = wormhole[num - 6][1].second;
@@ -58,30 +56,29 @@ void process(int y, int x, int dir, int cnt) {
 				wy = wormhole[num - 6][0].first, wx = wormhole[num - 6][0].second;
 			process(wy, wx, dir, cnt);
 		}
-		if (num < 6 && num > 0) {		//블럭 조건 처리
+		else if (num < 6 && num > 0) {		//블럭 조건 처리
 			dir = convDir[num][dir];	//블럭에 따라 방향 변환
 			process(ny, nx, dir, cnt + 1);	//점수 + 1
 		}
-		if (num == 0) {
+		else if (num == 0) {
 			process(ny, nx, dir, cnt);
 		}
 	}
 	else {
-		if (dir == 0) dir = 2;
-		else if (dir == 1) dir = 3;
-		else if (dir == 2) dir = 0;
-		else if (dir == 3) dir = 1;
-		ny = y + dy[dir], nx = x + dx[dir];
-		process(ny, nx, dir, cnt + 1);
+		cnt = cnt * 2 + 1;
+		ans = max(cnt, ans);
+		return;
 	}
 }
 void solution() {
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
 			sy = i, sx = j;	//시작점 저장
-			gameover = false;
 			if (!Map[i][j]) {
 				process(i, j, 0, 0);
+				process(i, j, 1, 0);
+				process(i, j, 2, 0);
+				process(i, j, 3, 0);
 			}
 		}
 	}
@@ -89,7 +86,7 @@ void solution() {
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	//freopen("swea_5650.in", "r", stdin);
+	freopen("swea_5650.in", "r", stdin);
 
 	int tc; cin >> tc;
 	for (int t = 1; t <= tc; t++) {
